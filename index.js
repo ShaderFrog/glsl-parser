645ab8f9f74ae6e3c3702b66b6e7b4699b965087
Andrew Ray <me@andrewray.me>
2021-08-08 22:23:36 -0700

    Testing next publish

---
+++ index.js
@@ -0,0 +1,4 @@
+const generator = require('./parser/generator.js');
+const parser = require('../dist/parser.js');
+
+module.exports = { generator, parser };
---
+++ preprocessor/index.js
@@ -0,0 +1,5 @@
+const generator = require('./generator.js');
+const preprocessor = require('./preprocessor.js');
+const parser = require('../dist/preprocssorParser.js');
+
+module.exports = { generator, preprocessor, parser };
