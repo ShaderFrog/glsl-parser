6be8dd67f00e8829460124bd3f7165c2dfc51a03
Andrew Ray <me@andrewray.me>
2021-08-08 22:37:23 -0700

    What is a verb, really?

--- preprocessor/index.js
+++ preprocessor/index.js
@@ -1,5 +1,5 @@
 const generate = require('./generator.js');
-const preprocessor = require('./preprocessor.js');
+const preprocess = require('./preprocessor.js');
 const parser = require('../dist/preprocssorParser.js');
 
-module.exports = { generate, preprocessor, parser };
+module.exports = { generate, preprocess, parser };
