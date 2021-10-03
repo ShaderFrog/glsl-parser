be618ed237117da5e4ac8c59caf11715457d7f3b
Andrew Ray <me@andrewray.me>
2021-10-03 16:00:56 -0700

    Testing copying files

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+set -e
+
+# Clean the output dir
+rm -rf dist/
 mkdir -p dist
-npx peggy -o dist/parser/parser.js parser/glsl-grammar.pegjs
-npx peggy -o dist/preprocessor/preprocessor-parser.js preprocessor/preprocessor-grammar.pegjs
+
+# Compile the typescript project
 npx tsc
+
+# Build the parers with peggy. Requires tsc to run first for the subfolders
+npx peggy -o dist/parser/parser.js parser/glsl-grammar.pegjs
+# Manualy copy in the type definitions
+cp parser/parser.d.ts dist/parser/parser.d.ts
+
+npx peggy -o dist/preprocessor/preprocessor-parser.js preprocessor/preprocessor-grammar.pegjs
+cp preprocessor/preprocessor-parser.d.ts dist/preprocessor/preprocessor-parser.d.ts
