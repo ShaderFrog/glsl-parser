bd6243fe8bc9537e4e7216c42068621271ad7529
Andrew Ray <me@andrewray.me>
2021-10-02 22:37:43 -0700

    I dunno

--- ast.ts
+++ ast.ts
@@ -1,1 +1,4 @@
+// Attempt to allow for import from glsl-parser/ast, combined with package
+// exports field. Does not currently work. Issue is closed though
+// https://github.com/microsoft/TypeScript/issues/33079
 export * from './core/ast';
