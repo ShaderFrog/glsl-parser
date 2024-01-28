b658a20e3bc2616b061a50f69fb0934f939ffce4
Andrew Ray <me@andrewray.me>
2024-01-27 19:24:33 -0800

    Build test

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ npx tsc
 npx peggy --cache -o dist/parser/parser.js src/parser/glsl-grammar.pegjs
 # Manualy copy in the type definitions
 cp src/parser/parser.d.ts dist/parser/
+cp src/error.d.ts dist/
 
 npx peggy --cache -o dist/preprocessor/preprocessor-parser.js src/preprocessor/preprocessor-grammar.pegjs
 cp src/preprocessor/preprocessor-parser.d.ts dist/preprocessor/preprocessor-parser.d.ts
