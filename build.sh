32c27264846431b1aa2be76575b66a3eb168f2de
Andrew Ray <me@andrewray.me>
2024-04-03 19:57:37 -0700

    Generating esm version of parser

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ mkdir -p dist
 # Compile the typescript project
 npx tsc
 
-npx peggy --cache -o dist/parser/parser.js src/parser/glsl-grammar.pegjs
+npx peggy --cache --format es -o dist/parser/parser.js src/parser/glsl-grammar.pegjs
 # Manualy copy in the type definitions
 cp src/parser/parser.d.ts dist/parser/
 cp src/error.d.ts dist/
 
-npx peggy --cache -o dist/preprocessor/preprocessor-parser.js src/preprocessor/preprocessor-grammar.pegjs
+npx peggy --cache --format es -o dist/preprocessor/preprocessor-parser.js src/preprocessor/preprocessor-grammar.pegjs
 cp src/preprocessor/preprocessor-parser.d.ts dist/preprocessor/preprocessor-parser.d.ts
