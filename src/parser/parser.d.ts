35fdd69809ebe058ab2605db2ab3e2567085a688
Andy Ray <me@andrewray.me>
2024-08-10 10:42:32 -0700

    Adding more exports from root

--- src/parser/parser.d.ts
+++ src/parser/parser.d.ts
@@ -17,7 +17,17 @@ export type ParserOptions = Partial<{
 // Allow to fetch util functions from parser directly. I'd like to inline those
 // functions directly in this file, but then the tests can't find it since jest
 // can't import from .d.ts files as there's no accompanying ts/js file
-export { renameBindings, renameFunctions } from './utils';
+export {
+  renameBinding,
+  renameBindings,
+  renameType,
+  renameTypes,
+  renameFunction,
+  renameFunctions,
+  debugEntry,
+  debugFunctionEntry,
+  debugScopes,
+} from './utils';
 
 export type Parse = {
   (input: string, options?: ParserOptions): Program;
